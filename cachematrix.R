84f52c39225dd77259d05695a9c0b26fa6f70a9d
vijaynalli <vijaynalli11@gmail.com>
2016-07-07 20:49:42 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,19 +3,16 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {m <- NULL
-  set <- function(y){
-     x <<- y  
-    m <<- NULL #store matrix in cache 
-   }
-   get <- function() x #get matrix
-   setInverse <- function(solve) m<<- solve #set inverse matrix
-   getInverse <- function() m #get inverse matrix
-   list(set = set, get = get,
-        setInverse = setInverse,
-        getInverse = getInverse)  ## create list of functions
-
-
+makeCacheMatrix <- function(x = matrix()) {
+	inverse <- NULL
+	set <- function(y) {
+		x <<- y
+		m <<- NULL
+	}
+	get <- function() x
+	setinverse <- function(i) inverse <- i
+	getinverse <- function() inverse
+	list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
@@ -23,13 +20,15 @@ makeCacheMatrix <- function(x = matrix()) {m <- NULL
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
-m <- x$getInverse()                 #query the x matrix's cache
-   if(!is.null(m)){                    #if there is a cache the inverse has been previously calculated
-     message("getting cached data")    # sent message indicating this is just cache 
-     return(m)                         # return the cache  
-   }
-   data <- x$get()                     # get the matrix used by makeCacheMatrix function 
-   m <- solve(data, ...)               # calculate the inverse of the matrix
-   x$setInverse(m)                     # store the inverse matrix in cache using the makeCacheMatrix set function
- 
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+        	message("getting cached inverse")
+        	return(inverse)
+        }
+        data <- x$get()
+        inverse <- solve(data, ...)
+        x$setinverse(inverse)
+        inverse
 }
+
+# data <- matrix(c(1, 1, 4, 0, 3, 1, 4, 4, 0), nrow=3, ncol=3)
